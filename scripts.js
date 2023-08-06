07586fcffe0d6bb23a77557d417cbdfc25074d01
Shelby Ashburn <76406378+ShelbyAshburn@users.noreply.github.com>
2023-08-06 19:26:44 -0400

    AI version

--- scripts.js
+++ scripts.js
@@ -1,2 +1,38 @@
 //12-4-2022
-//Hacked from many sources
+//
+
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+    taskInput.value = "";
+
+    if (taskText !== "") {
+        const taskList = document.getElementById("taskList");
+        const newTask = document.createElement("li");
+        newTask.innerText = taskText;
+        taskList.appendChild(newTask);
+    }
+}
+
+// Function to remove a task from the list
+function removeTask(event) {
+    const taskList = document.getElementById("taskList");
+    const taskItem = event.target;
+    taskList.removeChild(taskItem);
+}
+
+// Event listeners
+document.getElementById("addTaskBtn").addEventListener("click", addTask);
+document.getElementById("taskList").addEventListener("click", removeTask);
+document.getElementById("taskInput").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+
+
+
+
+
+
+
